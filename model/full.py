24010190258da9bdbb24ceda0d50b65102df67f5
froggagul <froggagul@gmail.com>
2020-12-08 18:41:52 +0900

    full model n_box, n_class

--- model/full.py
+++ model/full.py
@@ -4,7 +4,9 @@ from torch import nn
 from pretrain import Pretrain, ConvBlock
 
 class YOLO(nn.Module):
-    def __init__(self):
+    def __init__(self, n_box, n_class):
+        self.n_box = n_box
+        self.n_class = n_class
         super().__init__()
         self.layers = nn.ModuleList([
             Pretrain(),
@@ -14,8 +16,7 @@ class YOLO(nn.Module):
             ConvBlock(1024, 1024, 3, 1, 1),
         ])
         self.linear1 = nn.Linear(1024*7*7, 4096)
-        self.linear2 = nn.Linear(4096, 30*7*7)
-
+        self.linear2 = nn.Linear(4096, (5 * n_box + n_class)*7*7)
 
     def forward(self, x):
         for layer in self.layers:
@@ -24,10 +25,14 @@ class YOLO(nn.Module):
         x = self.linear1(x)
         x = self.linear2(x)
         x = torch.reshape(x, (-1, 7, 7, 30))
+        p_class = x[:, -self.n_class:]
+        print(p_class.shape)
+        for i in n_box:
+            
         return x    
 
 if __name__ == "__main__":
-    model = YOLO()
+    model = YOLO(n_box = 2, n_class = 20)
 
     test_input = torch.zeros([2, 3, 448, 448])
     test_output = model(test_input)
