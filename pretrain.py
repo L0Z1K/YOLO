d00e3ecc61e39b2cb85e7a8dc513afdb7b3b89e4
L0Z1K <qortmddbs731@naver.com>
2020-12-27 17:34:23 +0900

    update for dataset path and topk logic.

--- pretrain.py
+++ pretrain.py
@@ -8,37 +8,40 @@ import numpy as np
 
 debug = False
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-LOAD_PATH = '/content/gdrive/My Drive/YOLO/pretrain_40.pt'
-SAVE_PATH = '/content/gdrive/My Drive/YOLO/'
+LOAD_PATH = '/content/gdrive/Shareddrives/YOLO/pretrain_50.pt'
+SAVE_PATH = '/content/gdrive/Shareddrives/YOLO/'
 
 if not debug:
     assert device == 'cuda'
     '''not training on cuda! check GPU state'''
 
-def correct(output, target, topk=[1]):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        correct = pred.eq(target.view(-1, 1).expand(-1, maxk))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:,:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k)
-        return np.array(res, dtype = float)
+def accuracy(output, target):
+    batch_size = target.size(0)
+    _, pred = output.topk(5, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    error_1 = batch_size - correct[:1].view(-1).float().sum(0)
+    error_5 = batch_size - correct[:5].contiguous().view(-1).float().sum(0)
+    return [error_1, error_5]
 
 def test(test_loader, pretrain_model, topk = [1, 5]):
     pretrain_model.eval()
+    error_1 = 0
+    error_5 = 0
     res = np.zeros(len(topk), dtype=float)
     for x, y in test_loader:
         x, y = x.to(device), y.to(device)
         y_pred = pretrain_model(x)
-        res += correct(y_pred, y, topk)
-    res = res / len(test_loader.dataset) * 100
-    return topk, res
+        e1, e5 = accuracy(y_pred, y)
+        error_1 += e1
+        error_5 += e5
+    
+    error_1 = error_1 / len(test_loader.dataset) * 100
+    error_5 = error_5 / len(test_loader.dataset) * 100
+
+    return topk, [error_1, error_5]
+
+    
 
 
 def train(epoch, train_loader, pretrain_model, opt_pretrain_model):
@@ -89,18 +92,21 @@ if __name__ == "__main__":
     cnt = len(train_loader)
     total_epoch = 100 if not debug else 1
 
-    print(f'Train {"start" if start_epoch is 1 else "retstart"}! on {device}{"with debug on" if debug else ""}')
+    print(f'Train {"start" if start_epoch is 1 else "retstart"}! on {device}{" with debug on" if debug else ""}')
 
     start = time.time()
-    for epoch in range(start_epoch, total_epoch+1):
+    for epoch in range(start_epoch, start_epoch+total_epoch):
         train(epoch, train_loader, pretrain_model, opt_pretrain_model)
-        topk, accuracies = test(test_loader, pretrain_model)
+        topk, errors = test(test_loader, pretrain_model)
         print(f'test result at {epoch}')
-        for k, accuracy in zip(topk, accuracies):
-            print(f'top {k} accuracy: {accuracy}')
-        
-    torch.save({
-      'epoch': epoch,
-      'model_state_dict': pretrain_model.state_dict(),
-      'optimizer_state_dict': opt_pretrain_model.state_dict(),
-    }, f'{SAVE_PATH}pretrain_final.pt')
+        for k, error in zip(topk, errors):
+            print(f'top {k} error: {error}')
+
+    '''
+    if not debug:
+        torch.save({
+          'epoch': epoch,
+          'model_state_dict': pretrain_model.state_dict(),
+          'optimizer_state_dict': opt_pretrain_model.state_dict(),
+        }, f'{SAVE_PATH}pretrain_final.pt')
+    '''
